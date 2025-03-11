02795440da393bc594d0063c7242ba1d7396372a
sg182 <swarnamoyghosh@Swarnamoys-MacBook-Pro.local>
2025-03-11 15:37:49 -0500

    fixed bug

--- run_calc.sh
+++ run_calc.sh
@@ -1,8 +1,5 @@
 #!/bin/zsh
 
-#NX=8
-#NY=1
-
 OUTFILE="energy_XXZ.txt"
 
 echo "Delta\t\tEnergy" > "$OUTFILE"
@@ -12,7 +9,7 @@ echo "Delta\t\tEnergy" > "$OUTFILE"
 for Delta in $(seq -1.5 0.05 1.5); do
     #echo -e "Nx=$NX\nNy=$NY\nNsites=Nx*Ny\nDelta=$Delta" > parameter.py
     echo "$Delta"
-    sed -i '' "s/^Delta=.*/Delta=$Delta/" parameter.py
+    sed -i '' "s/^ *Delta *=.*/Delta=$Delta/" parameter.py  # * ensures if there is space in the parameter.py
     python3 XXZ_energy.py
 
 done 

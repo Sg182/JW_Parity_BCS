ecdc6332fa40ee53093756a46ad07d9c4af4ddac
sg182 <swarnamoyghosh@Swarnamoys-MacBook-Pro.local>
2025-03-19 14:51:36 -0500

    modified for readability

--- overlap_uv.py
+++ overlap_uv.py
@@ -1,31 +1,35 @@
 import numpy as np
 def bcs_overlap(theta,Nsites):   # computes overlap between two BCS states
     overlap = 1
+    
     for k in range(Nsites):
         overlap *= ((np.cos(theta[k]))**(2) + (np.sin(theta[k]))**(2))
     return overlap
 
 
-def S_x(theta,N,i):
-    prefactor = (1/2)*((np.cos(theta[i])*np.sin(theta[i]))+(np.cos(theta[i])*np.sin(theta[i])))/\
-        ((np.cos(theta[i])*np.cos(theta[i])+np.sin(theta[i])*np.sin(theta[i])))
+def S_x(theta,N,p):
+    cos_part_p = (np.cos(theta[p]))**2
+    sin_part_p = (np.sin(theta[p]))**2
+    prefactor =  (1/2)*(2*cos_part_p*sin_part_p)
     prefactor_Sx = prefactor*bcs_overlap(theta,N)
     return prefactor_Sx
-
 def Sz(theta,Nsites,i):     # calculates Sz for site i
-    prefactor_Sz = (1/2)*((-1*(np.cos(theta[i]))**(2) + (np.cos(theta[i]))**(2)))/\
-        ((np.cos(theta[i]))**(2) + (np.cos(theta[i]))**(2))
+    prefactor_Sz = (1/2)*((-1*(np.cos(theta[i]))**(2) + (np.sin(theta[i]))**(2)))/\
+        ((np.cos(theta[i]))**(2) + (np.sin(theta[i]))**(2))
     return bcs_overlap(theta,Nsites)*prefactor_Sz
 
 
-def S_zS_z(theta,N,p,q):
-    
-    prefactor = (1/4)*(((-1*(np.cos(theta[p]))**(2) + (np.sin(theta[p]))**(2)))*\
-                       ((-1*(np.cos(theta[q]))**(2) + np.sin(theta[q])**(2))))/\
-                       ((((np.cos(theta[p]))**2+(np.sin(theta[p]))**(2)))*\
-                        (((np.cos(theta[q]))**(2)+(np.sin(theta[q]))**(2))))
-    prefactor_SzS_z = prefactor*bcs_overlap(theta,N)
+def S_zS_z(theta,p,q):
+    cos_part_p = (np.cos(theta[p]))**2
+    sin_part_p = (np.sin(theta[p]))**2
+    cos_part_q =  (np.cos(theta[q]))**2
+    sin_part_q = (np.sin(theta[q]))**2  
+    numerator = ((-1*cos_part_p + sin_part_p)*(-1*cos_part_q + sin_part_q))
+    denominator = ((cos_part_p + sin_part_p)*(cos_part_q+sin_part_q))
+    prefactor = (1/4)*((numerator)/(denominator))
+    prefactor_SzS_z = prefactor
     return prefactor_SzS_z
+
 
 def SzSxSz(theta,N,p,i,q):    #Three body term
     prefactor = (1/8)*(((-1*(np.cos(theta[p]))**(2) + (np.cos(theta[p]))**(2)))*\

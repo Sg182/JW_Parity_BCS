2bed417b59c251e1757bf93855963d81ff175fff
sg182 <swarnamoyghosh@Swarnamoys-MacBook-Pro.local>
2025-10-30 17:43:34 -0500

    Added J1J2 for 1D OBC

--- overlap_uv.py
+++ overlap_uv.py
@@ -12,7 +12,29 @@ def S_x(theta,phi,N,p):
     sin_part_p = (np.sin(theta[p]))
     prefactor =  (cos_part_p*sin_part_p)*np.cos(phi[p])
     prefactor_Sx = prefactor#*bcs_overlap(theta,N)
-    return prefactor_Sx
+    return 0.5*(np.sin(2*theta[p]))*np.cos(phi[p])
+
+def S_xS_x(theta,phi,p,q):
+    return 0.25*(np.sin(2*theta[p]))*(np.sin(2*theta[q]))*(np.cos(phi[p]))*(np.cos(phi[q]))
+
+
+def S_zS_yS_yS_z(theta,phi,p,q,r,s):
+    SySy = (np.sin(2*theta[q]))*(np.sin(2*theta[r]))*(np.sin(phi[q]))*(np.sin(phi[r]))
+    SzSz = (np.cos(2*theta[p]))*(np.cos(2*theta[s]))
+    return 0.0625*SySy*SzSz
+
+def S_yS_yS_z(theta,phi,p,q,r):
+    SySy = 0.25*(np.sin(2*theta[p]))*(np.sin(2*theta[q]))*(np.sin(phi[p]))*(np.sin(phi[q]))
+    Sz = -0.5*(np.cos(2*theta[r]))
+    return SySy*Sz
+
+def S_zS_zS_zS_z(theta,phi,p,q,r,s):
+    return 0.0625*(np.cos(2*theta[p]))*(np.cos(2*theta[q]))*(np.cos(2*theta[r]))*(np.cos(2*theta[s]))
+
+def S_zS_zS_z(theta,phi,p,q,r):
+    return 0.125*(np.cos(2*theta[p]))*(np.cos(2*theta[q]))*(np.cos(2*theta[r]))
+
+
 def Sz(theta,Nsites,i):     # calculates Sz for site i
     prefactor_Sz = (1/2)*((-1*(np.cos(theta[i]))**(2) + (np.sin(theta[i]))**(2)))
          
@@ -72,6 +94,47 @@ def XXZ_1D_overlap(theta, phi, N, Delta):
                 E +=  Delta * S_zS_z(theta, p, r)
 
     return E
+    
+def J1J2_1D_overlap(theta,phi,N,J2):
+    E_J_1 = 0.0
+    E_J_2 = 0.0
+
+    for i in range(N-1):
+        E_J_1 += 0.5*S_x(theta, phi,N, i)
+
+    if True:
+        for i in range(N-1):
+            p = (i - 1)
+            r = (i + 1)
+            if p == -1:
+                E_J_1 += -S_xS_z(theta, phi, i, r)
+                E_J_1 +=  0.5* Sz(theta,N, r)
+            else:
+                E_J_1 += -2.0 * SzSxSz(theta, phi,N, p, i, r)
+                E_J_1 +=  S_zS_z(theta, p, r)
+
+    # For J2 part
+
+    for i in range(N-2):
+        j = i+1
+        E_J_2 += S_xS_x(theta,phi,i,j)
+    
+    if True:
+        for i in range(N-2):
+            p = i -1
+            r = i+1
+            s = i+2
+
+            if p == -1:
+                E_J_2 += 4*0.5*S_yS_yS_z(theta,phi,i,r,s)
+                E_J_2 += 4*0.5*S_zS_zS_z(theta,phi,i,r,s)
+            else:
+                E_J_2 += 4*S_zS_yS_yS_z(theta,phi,p,i,r,s)
+                E_J_2 += 4*S_zS_zS_zS_z(theta,phi,p,i,r,s)
+
+    return E_J_1 + E_J_2*J2
+
+
     
 
 '''def XXZ_1D_overlap(theta, phi, N, Delta, periodic=True):
